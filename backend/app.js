1fcda7b4e1cc582f039c0129ba819daad9ca3032
iciue <iciue.hao@gmail.com>
2019-11-20 15:11:00 +0800

    #

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
@@ -7,7 +8,7 @@ const {restaurant, desk} = require('./io-server')
 const userAccountMiddleware = require('./mw/user-account')
 const restaurantMiddleware = require('./mw/restaurant')
 
-const port = 8888
+const port = 3000
 
 const app = express()
 const server = http.createServer()
@@ -19,6 +20,7 @@ desk.attach(server)
 ;(async() => {
   const db = await require('./db/db-conn')
   global.db = db
+  console.log(`de connected`);
   server.listen(port, () => console.log(`app listening on ${port}`))
 })();
 
@@ -35,12 +37,18 @@ app.use(cors({
 
 app.use(cookieParser('my-secret'))
 
-app.use(express.static(__dirname + '/static'))
+
+app.use(express.static(__dirname + '/build'))
+app.use('/static', express.static(__dirname + '/static'))
 app.use('/upload', express.static(__dirname + '/upload'))
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
-
 app.use('/api', userAccountMiddleware)
 app.use('/api', restaurantMiddleware)
+
+app.get('*', function (request, response){
+  response.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+})
+
